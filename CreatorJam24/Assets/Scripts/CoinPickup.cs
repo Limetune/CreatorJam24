31b0a92adbefe2dc7c7b3401093fcef1fc64a8b1
vinlbs <161590272+vinlbs@users.noreply.github.com>
2024-05-31 13:51:17 +0200

    finally

--- CreatorJam24/Assets/Scripts/CoinPickup.cs
+++ CreatorJam24/Assets/Scripts/CoinPickup.cs
@@ -6,14 +6,13 @@ using TMPro;
 public class CoinPickup : MonoBehaviour
 {
     public int Coins = 0;
-    public TextMeshProUGUI PointText;
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Coin")
+        if (collision.gameObject.tag == "Player")
         {
             Coins++;
             Debug.Log("Coins" + Coins);
-            Destroy(collision.gameObject);
+            Destroy(gameObject);
         }
 
     }
