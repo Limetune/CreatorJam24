8f3df1aab4b429ea27ced3a09c37c6447bd75cc2
ProfessionalJoker <156196958+ProfessionalJoker@users.noreply.github.com>
2024-06-01 09:11:45 +0200

    Fixa lite med att ta upp pengar

---
+++ CreatorJam24/Assets/Scripts/Collision.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    public CoinManagment cm;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Coin")
+        {
+            cm.coinCount++;
+            Destroy(collision.gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
