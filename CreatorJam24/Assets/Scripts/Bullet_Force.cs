82786c4830625c6c6659094416684b5bcba5e3bc
ProfessionalJoker <156196958+ProfessionalJoker@users.noreply.github.com>
2024-06-02 11:29:17 +0200

    Pistol :)

---
+++ CreatorJam24/Assets/Scripts/Bullet_Force.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bulle_Force : MonoBehaviour
+{
+    private Vector3 mousePos;
+    private Camera mainCam;
+    private Rigidbody2D rb;
+    public float force;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        rb = GetComponent<Rigidbody2D>();
+
+        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 direction = mousePos - transform.position;
+        rb.velocity = new Vector2(direction.x, direction.y).normalized * force;
+
+        Vector3 rotation = transform.position - mousePos;
+        float rot = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rot + 90);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
